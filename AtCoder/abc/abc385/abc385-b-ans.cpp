e18b27201f6bd04dc6930f166d868686763107c1
Ririri6 <liaiwantian@gmail.com>
2024-12-30 14:05:47 +0900

    abc385-b

---
+++ AtCoder/abc/abc385/abc385-b-ans.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int H,W,X,Y;
+    cin>>H>>W>>X>>Y;
+
+    char S[H][W];
+
+    for(int i=0;i<H;i++){
+        for(int j=0;j<W;j++){
+            cin>>S[i][j];
+        }
+    }
+
+    string T;
+    cin>>T;
+    
+    int C=0;
+    X--;
+    Y--;
+    //cout<<S[X][Y]<<endl;
+    
+
+    for(size_t i=0;i<T.length();i++){
+         if(T[i]=='U' && S[X-1][Y]!='#'){
+              X=X-1;
+         }
+         if(T[i]=='D' && S[X+1][Y]!='#'){
+              X=X+1;
+         }
+        if(T[i]=='L' && S[X][Y-1]!='#'){
+              Y=Y-1;
+         }
+        if(T[i]=='R' && S[X][Y+1]!='#'){
+              Y=Y+1;
+         }
+         if(S[X][Y]=='@'){
+            C++;
+            S[X][Y]='.';
+         }
+
+        // cout<<S[X][Y]<<endl;
+    }
+
+    cout<<X+1<<" "<<Y+1<<" "<<C<<endl;
+}
---
+++ AtCoder/abc/abc385/abc385-b.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int H,W,X,Y;
+    cin>>H>>W>>X>>Y;
+
+    char S[H][W];
+
+    for(int i=0;i<H;i++){
+        for(int j=0;j<W;j++){
+            cin>>S[i][j];
+        }
+    }
+
+    string T;
+    cin>>T;
+    
+    int C=0;
+    X--;
+    Y--;
+    //cout<<S[X][Y]<<endl;
+    
+    for(size_t i=0;i<T.length();i++){
+         if(T[i]=='U' && S[X-1][Y]!='#'){
+              X=X-1;
+              if(S[X-1][Y]=='@'){
+                C++;
+                S[X-1][Y]='.';
+              }
+         }
+         else if(T[i]=='D' && S[X+1][Y]!='#'){
+              X=X+1;
+              if(S[X+1][Y]=='@'){
+                C++;
+                S[X+1][Y]='.';
+              }
+         }
+         else if(T[i]=='L' && S[X][Y-1]!='#'){
+              Y=Y-1;
+              if(S[X][Y-1]=='@'){
+                C++;
+                S[X][Y-1]='.';
+              }
+         }
+         else if(T[i]=='R' && S[X][Y+1]!='#'){
+              Y=Y+1;
+              if(S[X][Y+1]=='@'){
+                C++;
+                S[X][Y+1]='.';
+              }
+         }
+
+        // cout<<S[X][Y]<<endl;
+    }
+
+    cout<<X+1<<" "<<Y+1<<" "<<C<<endl;
+}
