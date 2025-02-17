eecd50d076caf33e82b0f3e192b96a2f5781a31c
Ririri6 <liaiwantian@gmail.com>
2025-02-17 14:29:13 +0900

    abc393-b

--- AtCoder/abc/abc393/abc393-b.cpp
+++ AtCoder/abc/abc393/abc393-b.cpp
@@ -5,8 +5,17 @@ using namespace std;
 int main(){
     string S;
     cin>>S;
+    int result=0;
 
     for(int i=0;i<S.length();i++){
-        
+        for(int j=i+1;j<S.length();j++){
+            for(int k=j+1;k<S.length();k++){
+                if(j-i==k-j && S[i]=='A' && S[j]=='B' && S[k]=='C'){
+                     result++;
+                }
+            }
+        }
     }
+
+    cout<<result<<endl;
 }
