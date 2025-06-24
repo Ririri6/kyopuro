0d1ba54dfde6d6a1aeeedb852f14fb9ae7afcba8
Ririri6 <liaiwantian@gmail.com>
2025-06-24 16:59:44 +0900

    NoviSteps-6Q-ソート

---
+++ AtCoder/NoviSteps/6Q/B-Mixjuice.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int N,K;
+  cin>>N>>K;
+
+  vector<int> p(N);
+  for(int i=0;i<N;i++){
+    cin>>p[i];
+  }
+
+  int ans=0;
+  sort(p.begin(),p.end());
+  for(int i=0;i<K;i++){
+    ans+=p[i];
+  }
+
+  cout<<ans<<endl;
+  
+}
