690c57695205064e016816c105a5f012dc1111fa
Ririri6 <liaiwantian@gmail.com>
2025-10-05 23:00:10 +0900

    精進

--- AtCoder/精進/C-RotateColoredSubsequence.cpp
+++ AtCoder/精進/C-RotateColoredSubsequence.cpp
@@ -1,15 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
-   long long N,M;
-   cin>>N>>M;
-   string S;
-   cin>>S;
-   vector<long long> C(N);
-   for(int i=0;i<N;i++){
-    cin>>C[i];
-   }
+    int N, M;
+    cin >> N >> M;
+    string S;
+    cin >> S;
+    vector<int> C(N);
+    for (int i = 0; i < N; i++) cin >> C[i];
 
+    // 各グループごとの文字列を構築
+    vector<string> groups(M);
+    for (int i = 0; i < N; i++) {
+        groups[C[i] - 1].push_back(S[i]);
+    }
 
+    // グループごとに「右シフト」するが、実際には末尾→先頭だけやる
+    for (int g = 0; g < M; g++) {
+        if (!groups[g].empty()) {
+            char last = groups[g].back();
+            groups[g].pop_back();
+            groups[g].insert(groups[g].begin(), last);
+        }
+    }
+
+    // 答えを構築
+    string ans;
+    vector<int> pos(M, 0); // 各グループの現在位置
+    ans.reserve(N);
+    for (int i = 0; i < N; i++) {
+        int g = C[i] - 1;
+        ans.push_back(groups[g][pos[g]]);
+        pos[g]++;
+    }
+
+    cout << ans << endl;
 }
