9b4472baefe0cfb9712d8aec0574a0d96977aeab
Ririri6 <liaiwantian@gmail.com>
2025-10-31 13:59:37 +0900

    精進．bit全探索を実装できてうれしい！！

--- AtCoder/精進/C-Coverage.cpp
+++ AtCoder/精進/C-Coverage.cpp
@@ -4,5 +4,32 @@ using namespace std;
 int main() {
    int N,M;
    cin>>N>>M;
-   
+
+   vector<int> C(M);
+   vector<vector<int>> a(10, vector<int>(10));
+   for(int i=0;i<M;i++){
+      cin>>C[i];
+      for(int j=0;j<C[i];j++){
+         cin>>a[i][j];
+      }
+   }
+  
+  int ans=0;
+  for(int bit=0;bit<1<<M;bit++){
+   set<int> A;
+   for(int i=0;i<M;i++){
+      for(int j=0;j<C[i];j++){
+         if(bit &(1<<i)){
+            A.insert(a[i][j]);
+         }
+      }
+   }
+   if(A.size()==N){
+      ans++;
+   }
+   A.clear();
+  }
+
+  cout<<ans<<endl;
+
 }
